458afb70a6094f2c28d7ce72ed1aa1e77bebe8d0
Charles <egualecharles@gmail.com>
2022-09-30 04:21:46 +0100

    add query to get all products

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,31 @@ import { ApolloServer, gql } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
 import http from 'http';
+import {data} from './data.js';
 
 const typeDefs = gql `
     type Query {
-        hello: String
+        products: [Product!]!
+        product (name: ID!): Product
+    }
+
+    type Product {
+        name: String!
+        price: Int!
+        onSale: Boolean!
+        description: String!
     }
 `
-
+ 
 const resolvers = {
     Query: {
-        hello: () => {
-            return "hello"
-        }
+        products: () => data,
+        product : (parent, args, context) => {
+            const productName = args.name
+            const product = data.find((p) => p.name === productName)
+            if (!product) return null
+            return product
+        } 
     }
 }
 
@@ -23,11 +36,11 @@ async function startApolloServer(typeDefs, resolvers) {
   const httpServer = http.createServer(app);
 
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs, // how data is going to look like or a schema
+    resolvers, // function that's responsible for populating the data for a single field in your schema or function fired
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-
+ 
   await server.start();
 
   server.applyMiddleware({ app });
